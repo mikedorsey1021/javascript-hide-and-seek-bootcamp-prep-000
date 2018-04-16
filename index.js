cd3cf2b7c9f5d6616969aefd50594e8d659bf24e
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-16 14:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function getFirstSelector (selector){
 function nestedTarget (nested){
   return document.querySelector('#nested .target');
 }
+
+function increaseRankBy (n){
+  const lis = document.getElementById('app').querySelector('.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = (i + n).toString();
+  }
+}
