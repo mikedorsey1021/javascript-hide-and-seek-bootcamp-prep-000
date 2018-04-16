ce5377835620a722e9c19df0f008c0eb5276274c
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-16 15:04:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector (selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget (nested){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy (n){
+  const lis = document.getElementById('app').querySelectorAll('.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  }
+}
+
+function deepestChild (){
+  return document.querySelector('#grand-node div div div div')
+}
