43c01454d88ee058fcb6ae761eae6a2bdf588dd4
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-16 15:02:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ function increaseRankBy (n){
   const lis = document.getElementById('app').querySelectorAll('.ranked-list li');
 
   for (let i = 0; i < lis.length; i++){
-
-    let start =
     lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
   }
+}
 
+function deepestChild (){
+  
 }
